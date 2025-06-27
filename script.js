bf5801b16158cd8fa1daf4dee6e4ddbae3b360fd
Kexin <kkc2160@columbia.edu>
2025-06-27 05:23:28 +0100

    Added mode button opacity transition

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 document.getElementById("mode-button").addEventListener("click", () => {
     let body = document.body;
     let button = document.getElementById("mode-button");
-    if (body.classList.contains("day")) {
-        body.classList.add("night");
-        body.classList.remove("day");
-        button.textContent = "⏾";
-        console.log("Night mode is active");
-    }
-    else {
-        body.classList.remove("night");
-        body.classList.add("day");
-        button.textContent = "☀";
-        console.log("Day mode is active");
-    }
+    button.style.opacity = '0';
+
+    setTimeout(() => {
+        if (body.classList.contains("day")) {
+            body.classList.add("night");
+            body.classList.remove("day");
+            button.textContent = "⏾";
+            console.log("Night mode is active");
+        }
+        else {
+            body.classList.remove("night");
+            body.classList.add("day");
+            button.textContent = "☀";
+            console.log("Day mode is active");
+        }
+
+        button.style.opacity = '1';
+    }, 150)
 });
